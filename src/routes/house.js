05352786128bfa1c566a865556ac311b1e8ec130
Roman <rdubinkin@gmail.com>
2020-10-28 05:57:15 -0400

    working on /house handles logic

--- src/routes/house.js
+++ src/routes/house.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: '../../public/img' });
+const House = require('../models/House');
 
 router.get('/', (req, res) => {
   // otobrazhajutsja vse doma
+});
+
+router.get('/new', (req, res) => {
+  // otobrazhetsja vse formy dlja wablona dom
 });
 
 router.get('/:id', (req, res) => {
@@ -17,8 +26,11 @@ router.get('/:id/delete', (req, res) => {
   //chistim bazu dannyh?
 });
 
-router.post('/:id/save', (req, res) => {
-  //zapisyvaem v bazu danny
+router.post('/:id/save', upload.single('image_uploads'), async (req, res) => {
+  fs.renameSync(req.file.path, path.join(process.env.PWD, `public/img/${req.file.originalname}`));
+  const { price, name, description } = req.body;
+  const house = new House({ price, name, description });
+  await house.save();
 });
 
 
