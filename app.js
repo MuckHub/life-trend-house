5a6f938be2ee09dd2d4cfdff824da000b8a1e8f8
Roman <rdubinkin@gmail.com>
2020-10-28 08:06:49 -0400

    release in-between

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const multer = require('multer');
 const app = express();
 const indexRouter = require('./src/routes/index');
 const houseRouter = require('./src/routes/house');
+const methodOverride = require('method-override')
 const dbConnect = require('./src/config/dbConnect');
 
 
 dbConnect();
 const PORT = process.env.PORT || 3000
+
+// Allows you to use PUT, DELETE with forms.!!!!!
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'src/views'));
