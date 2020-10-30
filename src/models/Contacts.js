673f5315f281ba7265d57164c8d9ac048e9c00b9
Aleksei Vesselko <aleksei.vesselko@gmail.com>
2020-10-30 13:08:18 +0000

    Contacts

--- src/models/Contacts.js
+++ src/models/Contacts.js
@@ -14,6 +14,4 @@ const contactsSchema = new mongoose.Schema({
     type: String,
   },
 });
-
-
 module.exports = mongoose.model('Contacts', contactsSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const House = require('../models/House');
+const Contacts = require('../models/Contacts');
 
 router.route('').get(async (req, res) => {
   const houses = await House.find().limit(3).lean();
   res.render('main', { houses });
 });
 
-router.get('/contacts', (req, res) => {
-  res.render('contacts');
+router.get('/contacts', async (req, res) => {
+  const contact = await Contacts.findOne().limit(1).lean();
+  res.render('contacts', { contact });
+});
+
+router.get('/contacts/edit', async (req, res) => {
+  const contact = await Contacts.findOne().lean();
+  console.log(contact);
+  res.render('contactsEdit', { contact });
+});
+router.post('/contacts/edit', async (req, res) => {
+  console.log(req.body);
+  const { name, tel, email, address } = req.body;
+  await Contacts.updateOne({}, { name, tel, email, address });
+  res.redirect('/contacts');
 });
 
 router.get('/team', (req, res) => {
